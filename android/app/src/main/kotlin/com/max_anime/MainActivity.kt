62b830a3a272c3f7dd3352b361d8a09f65d25a4b
jho3r <jhoer.perez@gmail.com>
2021-11-24 00:34:57 -0500

    Primera parte de la aplicacion, pantlla de login y register

---
+++ android/app/src/main/kotlin/com/max_anime/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.max_anime
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/app.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:max_anime/ui/pages/content.dart';
+import 'package:max_anime/ui/pages/login.dart';
+import 'package:max_anime/ui/pages/register.dart';
+
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Red Max Anime',
+      theme: ThemeData(brightness: Brightness.dark
+          //primarySwatch: Colors.blueGrey,
+          ),
+      initialRoute: '/',
+      routes: {
+        // When navigating to the "/" route, build the FirstScreen widget.
+        '/': (context) => const LoginPage(
+              title: 'Login',
+            ),
+        '/register': (context) => const RegisterPage(
+              title: 'Registro',
+            ),
+        '/content': (context) => const ContentPage(title: 'Posts'),
+      },
+    );
+  }
+}
